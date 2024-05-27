28ce705db753607b6f2291d386904ede2e3d0d7f
Ayda <aydaadl657@gmail.com>
2024-05-27 18:15:10 +0330

    RPS file created

---
+++ RPS.py
@@ -0,0 +1,32 @@
+import random 
+
+while True:
+    you = input("Enter a choice(Rock, Paper, scissors):")
+    choose =  ["Rock","Paper","Scissors"]
+    computer = random.choice(choose)
+    print("\n mychoose{you}, yourchoose{computer}\n")
+
+    if you == computer:
+        print(f"both players selected{you}.equal")
+    elif you == "Rock":
+        if computer == "scissors":
+            print("you win")
+        else:
+            print("computer win")    
+    elif you == "Paper": 
+        if computer == "Rock":
+            print("you win") 
+    else:
+        print("computer win")
+    Elif you == "scissors": 
+        if computer == "Paper":
+            print("you win") 
+    else:
+        print("computer win")
+       
+    
+    play_again = input("play again?(y/n):")
+    if play_again.lower() != "yes":
+        break
+        
+        
